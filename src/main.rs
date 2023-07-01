2bafa35424989faeaa280153c6b600a1cadf2832
lancenonce <lancendavis47@gmail.com>
2023-07-01 11:55:21 -0400

    panic handler and main function with VGA spice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Let's create the LDOS
+#![no_std]
+
+use core::panic::PanicInfo;
+
+/// called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in b"Hello LD!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
